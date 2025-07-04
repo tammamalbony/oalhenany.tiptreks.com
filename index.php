e76a83af4c34069cb602633a6b53c5b384eb0203
Wafa AF <WafaAbufaour@gmail.com>
2025-07-04 04:36:48 -0700

    main

---
+++ index.php
@@ -0,0 +1,419 @@
+<?php require_once("Data.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php echo $head; ?>
+</head>
+
+<body>
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <?php echo $nav; ?>
+  </header><!-- End Header -->
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center" style="">
+    <div class="hero-container" data-aos="fade-in" dir="rtl">
+      <h1>عمران الحيناني</h1>
+      <p> <span class="typed" dir="rtl"
+          data-typed-items="مهندس زراعي , ماجستير في تصميم وتنسيق الحدائق , دكتوراه في تخطيط و تصميم المواقع العامة"></span>
+      </p>
+    </div>
+  </section><!-- End Hero -->
+  <main id="main">
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+        <div class="section-title">
+          <h2>الرؤية</h2>
+          <div dir="rtl">
+            <i class="bi bi-caret-left-fill text-success"></i><span> السعي الدائم لتطوير تقنيات وأساليب جديدة لتصميم
+              وتنفيذ المشاريع الزراعية والمناظر الطبيعية
+              المستدامة التي تحسن جودة الحياة في المدن وتوفر بيئة صحية وجميلة للمجتمعات المحلية.
+              كما أريد تحديد المعايير الدولية لتصميم وتنفيذ المشاريع الزراعية المستدامة</span></br>
+            <i class="bi bi-caret-left-fill text-success"></i><span> وتطوير نظامي الخاص لتدريب المهنيين
+              في هذا المجال وتوفير بيئة عمل محفزة للإبداع والإبتكار.</span></br>
+            <i class="bi bi-caret-left-fill text-success"></i><span> أسعى لتحسين جودة الحياة للمجتمعات المحلية ونشر
+              الوعي بأهمية الزراعة المستدامة والمناظر الطبيعية في
+              المدن .</span></br>
+            <i class="bi bi-caret-left-fill text-success"></i><span> ابتكار نظم زراعية حديثة توفر التكاليف وتحافظ على
+              موارد الأرض.
+              أريد الوصول إلى وضع قواعد صحيحة لتصميم كافة مواقع اللاندسكيب واستخدام النباتات المناسبة لكل منها وطرق الري
+              الأنسب لتوفير تكاليف الصيانة الباهضة وزيادة الجمالية والفائدة البيئية منها.</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-right text-white bg" dir="rtl" id="About_Edu">
+            <style>
+              #About_Edu h3,
+              #About_Edu h4,
+              #About_Edu h5 {
+                color: #fff;
+                font-family: "Raleway", sans-serif;
+              }
+
+              #About_Edu {
+                background-color: #043042;
+                border-top-right-radius: 25px;
+                border-top-left-radius: 25px;
+                border: #288008 solid 5px !important;
+              }
+            </style>
+            <h4><i class="bi bi-mortarboard mx-2 text-success"></i>دكتوراه في الهندسة الزراعية باختصاص تخطيط وتصميم
+              المناطق الخضراء في الأحياء السكنية باستخدام نظم
+              المعلومات الجغرافية GIS</h4>
+            <h4> <i class="bi bi-vector-pen mx-2 text-success"></i> ماجستير في تصميم وتنسيق الحدائق</h4>
+            <h4><i class="bi bi-book mx-2 text-success"></i> بكالوريوس في الهندسة الزراعية اختصاص علوم البستنة</h4>
+          </div>
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="./Pro/profile.jpg" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+
+            <!-- <h3 class="text-center">دكتوراه في تخطيط و تصميم المواقع العامة &amp; بكالوريوس في الهندسة الزراعية اختصاص
+              علوم البستنة
+              &amp; ماجستير في تصميم
+              وتنسيق الحدائق</h3> -->
+            <ul class="fst-italic" dir="rtl">
+              <li> &#9679; مدرب في عدة محافظات سورية وفي الإمارات العربية المتحدة ( دبي)</li>
+              <li> &#9679; مدير مكتب Land Experts لتصميم وتنفيذ كافة أعمال اللاندسكيب .</li>
+              <li> &#9679; مهندس فني في شركة بدار للزراعة والتجارة للإشراف على المشاريع الزراعية وتسويق المستلزمات
+                الزراعية.</li>
+            </ul>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul dir="rtl">
+                  <li><i class="bi bi-wifi"></i> <strong>الموقع : </strong><a href="http://oalhenany.com"> <span>
+                        www.oalhenany.com</span></a>
+                  </li>
+                  <li><i class="bi bi-whatsapp"></i> <strong>واتس أب : </strong> <span dir="ltr"><a
+                        href="https://wa.me/971581524939">+971581524939</a></span></li>
+                  <li><i class="bi bi-telephone"></i> <strong>الموبايل : </strong> <span dir="ltr"><a
+                        href="tel:00963994763404">+963994763404</a></span></li>
+                  <li><i class="bi bi-map"></i> <strong>الإقامة :</strong> <span> سوريا , دمشق</span></li>
+                  <li><i class="bi bi-translate"></i> <strong>اللغة العربية :</strong> <span>لغتي الأصلية </span>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul dir="rtl">
+                  <li><i class="bi bi-chevron-left"></i> <strong>الميلاد : </strong> <span> 20/2/1991</span></li>
+                  <li><i class="bi bi-chevron-left"></i> <strong>الجنس:</strong> <span>ذكر</span></li>
+                  <li><i class="bi bi-chevron-left"></i> <strong>البريد الإلكتروني:</strong>
+                    <span>oalhenany@gmail.com</span>
+                  </li>
+                  <li><i class="bi bi-chevron-left"></i> <strong>العمل كمستقل:</strong> <span>متوفر</span></li>
+                  <li><i class="bi bi-chevron-left"></i> <strong> اللغة الإنكليزية :</strong> <span>جيدة </span></li>
+                </ul>
+              </div>
+            </div>
+            <p dir="rtl">
+              يعتبر تحقيق الأهداف هي الخطوة الأساسية لتحديد الخطوات التي يجب اتخاذها لتحقيق رؤيتي.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+    <!-- ======= Facts Section ======= -->
+    <section id="facts" class="facts">
+      <div class="container">
+        <div class="section-title">
+          <h2>أرقام و إحصائيات</h2>
+          <p>توضح الأعمال و الإنجازات </p>
+        </div>
+        <div class="row no-gutters">
+          <?php foreach ($counterData as $item) {
+            echo '<div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up">
+                          <div class="count-box">
+                            <i class="bi ' . $item['icon'] . '"></i>
+                            <span data-purecounter-start="' . $item['start'] . '" data-purecounter-end="' . $item['end'] . '" data-purecounter-duration="1" class="purecounter"></span>
+                            <p><strong>' . $item['label'] . '</strong></p>
+                          </div>
+                        </div>';
+          } ?>
+        </div>
+      </div>
+    </section><!-- End Facts Section -->
+
+    <!-- ======= Skills Section ======= -->
+    <section id="skills" class="skills section-bg">
+      <div class="container">
+        <style>
+          #skills {
+            border-top-right-radius: 25px;
+            border-top-left-radius: 25px;
+          }
+        </style>
+        <div class="section-title">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                <h2>المهارات</h2>
+              </div>
+              <div class="col-md-8">
+                <ul dir="rtl">
+                  <li>خبرة في تصميم مخططات اللاندسكيب باستخدام أحدث البرامج الهندسية المتخصصة وتنفيذ الأعمال والإشراف
+                    على متابعة العمل وإعداد دفاتر الشروط وجداول الكميات والمقايسة للمشروع وبرامج أعمال الصيانة.</li>
+                  <li> خبرة في مجال دراسة وإدارة مشاريع الفطر المحاري.</li>
+                  <li> دراسة المشاريع الهندسية وتصميم وتنفيذ كافة أعمال اللاندسكيب</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div class="row skills-content">
+
+          <div class="col-lg-6" data-aos="fade-up">
+            <?php foreach ($skills[1] as $skill => $percentage) {
+              echo '<div class="progress">
+                      <span class="skill">' . $skill . ' <i class="val">' . $percentage . '%</i></span>
+                        <div class="progress-bar-wrap">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="' . $percentage . '" aria-valuemin="0" aria-valuemax="100">
+                          </div>
+                      </div>
+                    </div>';
+            } ?>
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            <?php foreach ($skills[2] as $skill => $percentage) {
+              echo '<div class="progress">
+                      <span class="skill">' . $skill . ' <i class="val">' . $percentage . '%</i></span>
+                        <div class="progress-bar-wrap">
+                          <div class="progress-bar" role="progressbar" aria-valuenow="' . $percentage . '" aria-valuemin="0" aria-valuemax="100">
+                          </div>
+                      </div>
+                    </div>';
+            } ?>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Skills Section -->
+
+    <!-- ======= Resume Section ======= -->
+    <section id="resume" class="resume">
+      <div class="container">
+
+        <div class="section-title">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                <h2>السيرة الذاتية</h2>
+              </div>
+              <div class="col-md-8">
+                <ul dir="rtl">
+                  <li>خبرة في مجال تخطيط وتصميم المناطق الخضراء والفراغات العمرانية وتنفيذ التصميمات على أرض الوقع
+                    ومتابعة الأعمال والإشراف عليها.
+                  </li>
+                  <li>تصميم وتنفيذ شبكات الري ووضع برامج زمنية لصيانة الحدائق العامة والخاصة وبرامج إدارة الحدائق.
+                  </li>
+                  <li>خبرة تدريب عدة مجالات في الهندسة الزراعية والتنمية البيئية المستدامة.
+                  </li>
+
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h3 class="resume-title">الملخص</h3>
+            <div class="resume-item pb-0">
+              <h4>عمران الحيناني </h4>
+              <p><em>عربي سوري</em></p>
+              <ul>
+                <li>ماجستير في تصميم الحدائق</li>
+                <li>مدرب معتمد </li>
+                <li>وكيل شركة اليسر للإرتقاء العمراني  في السويداء</li>
+                <li>دراسة وإدارة مشاريع الفطر المحاري</li>
+                <li>مشرف على مشاريع عديدة في أغلب المحافظات السورية</li>
+              </ul>
+            </div>
+
+            <h3 class="resume-title">الدراسة</h3>
+            <?php
+            foreach ($education as $item) {
+              echo '<div class="resume-item">
+                      <h4>' . $item["title"] . '</h4>
+                      <h5>' . $item["year"] . '</h5>
+                      <p><em>' . $item["university"] . '</em></p>
+                      <p>' . $item["description"] . '</p>
+                    </div>';
+            }
+            ?>
+            <h3 class="resume-title">الدورات التدريبية</h3>
+            <?php
+            foreach ($courses as $course) {
+              echo '<div class="resume-item">
+                      <h4>' . $course["title"] . '</h4>
+                      <h5>' . $course["date"] . '</h5>
+                      <p><em>' . $course["organization"] . '</em></p>
+                      <p>' . $course["description"] . '</p>
+                    </div>';
+            }
+            ?>
+          </div>
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+            <h3 class="resume-title">الخبرات</h3>
+            <?php foreach ($expersinces as $item) {
+              echo '<div class="resume-item">
+                            <h4>' . $item["location"] . '</h4>
+                            <h5>' . $item["date"] . '</h5>
+                            <p><em>' . $item["title"] . '</em></p>';
+              if (!empty($item["description"])) {
+                echo '<ul dir="rtl">';
+                foreach ($item["description"] as $desc) {
+                  echo '<li>' . $desc . '</li>';
+                }
+                echo '</ul>';
+              }
+              echo '</div>';
+            } ?>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>الأعمال</h2>
+          <p>عينة من الأعمال الموثقة لتعكس جودة العمل و الدقة بالتنفيذ و الأسس المتبعة بالتخطيط <br>
+
+          </p>
+        </div>
+
+        <div class="row" data-aos="fade-up">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">الكل</li>
+              <?php foreach ($Cats as $cat => $catname) { 
+              echo "<li data-filter='.filter-".$catname["en"]."'>".$catname["dis"]."</li>";
+             } ?>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
+
+          <?php foreach ($Main as $G => $item) { ?>
+          <div class="col-lg-4 col-md-6 portfolio-item filter-<?php echo $item['T'] ?>">
+            <div class="portfolio-wrap">
+              <div class="card mb-3">
+                <img src="<?php echo $rootlink ."/". $item['I'] . ".jpg" ; ?>" class="img-fluid" alt="">
+                <div class="card-body">
+                  <h5 class="card-title">
+                    <?php echo $item['S']; ?>
+                  </h5>
+                  <p class="card-text"></p>
+                  <p class="card-text"><small class="text-body-secondary"></small></p>
+                </div>
+              </div>
+              <div class="portfolio-links ">
+                <a href="<?php echo $rootlink ."/". $item['I'] . " .jpg"; ?>" data-gallery="portfolioGallery"
+                  class="portfolio-lightbox" data-title="
+                  <?php echo $item['S'] ?>"><i class="bi bi-zoom-in"></i>
+                </a>
+                <a href="portfolio-details.php?G=<?php echo $G; ?>" title="More Details"><i
+                    class="bx bx-link"></i></a>
+              </div>
+            </div>
+          </div>
+          <?php
+          } ?>
+
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container">
+        <div class="section-title">
+          <h2>معلومات عامة</h2>
+          <p></p>
+        </div>
+        <div class="row">
+          <?php
+          foreach ($services as $service):
+            ?>
+          <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
+            <div class="icon"><i class="bi <?php echo $service['icon']; ?>"></i></div>
+            <h4 class="title"><a href="<?php echo $service['link']; ?>">
+                <?php echo $service['title']; ?>
+              </a></h4>
+            <p class="description">
+              <?php echo $service['description']; ?>
+            </p>
+          </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Testimonials Section ======= -->
+    <?php
+
+    ?>
+    <section id="testimonials" class="testimonials section-bg">
+      <div class="container">
+        <div class="section-title">
+          <h2>الأهداف</h2>
+          <p>يعتبر تحقيق الأهداف هي الخطوة الأساسية لتحديد الخطوات التي يجب اتخاذها لتحقيق رؤيتي</p>
+        </div>
+        <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+          <div class="swiper-wrapper">
+            <?php foreach ($Goals as $Goal): ?>
+            <div class="swiper-slide">
+              <div class="testimonial-item" data-aos="fade-up">
+                <p>
+                  <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                  <?php echo $Goal['quote']; ?>
+                  <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+                </p>
+                <i class="bi bi-<?php echo $Goal['circle']; ?>-circle"></i>
+                <h3>
+                  <?php echo $Goal['title']; ?>
+                </h3>
+                <h4>
+                  <?php echo $Goal['subtitle']; ?>
+                </h4>
+              </div>
+            </div><!-- End testimonial item -->
+            <?php endforeach; ?>
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+      </div>
+    </section><!-- End Testimonials Section -->
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <?php echo $footer; ?>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <?php echo $end; ?>
+
+</body>
+
+</html>
---
+++ portfolio-details.php
@@ -0,0 +1,252 @@
+<?php require_once("Data.php");
+
+$G = 0;
+
+if (isset($_GET['G'])) {
+  $G = $_GET['G'];
+
+} else {
+  header("Location: index.php");
+  exit;
+}
+
+if ($G == 0) {
+  header("Location: index.php");
+  exit;
+}
+
+
+$portfolioItems = [];
+if ($G == 1) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(1, 1));
+} elseif ($G == 2) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(1, 2));
+} elseif ($G == 3) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'D', array(2, 1));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 1));
+} elseif ($G == 4) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'N', array(2, 2, 1));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'M', array(2, 2, 1));
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(2, 2, 2));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 2, 1));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'W', array(2, 2, 1));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'D', array(2, 2, 1));
+
+} elseif ($G == 5) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 3));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'W', array(2, 3));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 3));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'D', array(2, 3));
+} elseif ($G == 6) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 4));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 4));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'R', array(2, 4));
+} elseif ($G == 7) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'D', array(2, 5));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 5));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 5));
+} elseif ($G == 8) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(2, 6));
+} elseif ($G == 9) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'D', array(2, 7, 1));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 7, 1));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 7, 1));
+
+  $portfolioItems = addPortfolioItem($portfolioItems, 'D', array(2, 7, 2));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 7, 2));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 7, 2));
+} elseif ($G == 10) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 8));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 8));
+} elseif ($G == 11) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'D', array(2, 9));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 9));
+} elseif ($G == 12) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 10));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 10));
+} elseif ($G == 13) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 11));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 11));
+} elseif ($G == 14) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(2, 12));
+} elseif ($G == 15) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'D', array(2, 13));
+} elseif ($G == 16) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 14));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 14));
+} elseif ($G == 17) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'D', array(2, 15));
+} elseif ($G == 18) {
+  $portfolioItems = addPortfolioItem($portfolioItems, 'D', array(2, 16));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'B', array(2, 16));
+  $portfolioItems = addPortfolioItem($portfolioItems, 'A', array(2, 16));
+} elseif ($G == 19) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(2, 17));
+} elseif ($G == 20) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(3, 1));
+} elseif ($G == 21) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(3, 2));
+} elseif ($G == 22) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(3, 3));
+} elseif ($G == 23) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(3, 4));
+} elseif ($G == 24) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(3, 5));
+
+} elseif ($G == 25) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(3, 6));
+} elseif ($G == 26) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(3, 7));
+} elseif ($G == 27) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(4, 1));
+} elseif ($G == 28) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(4, 2));
+} elseif ($G == 29) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(4, 3));
+} elseif ($G == 30) {
+  $portfolioItems = addPortfolioItem($portfolioItems, '', array(4, 4));
+} else {
+  header("Location: index.php");
+  exit;
+}
+
+
+$value = reset($portfolioItems);
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php echo $head; ?>
+</head>
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <?php echo $nav; ?>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>
+            <?php echo $Main[$G]['S']; ?>
+          </h2>
+          <ol>
+            <li><a href="index.php">Home</a></li>
+            <li>
+              <?php echo $Main[$G]['S']; ?>
+            </li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Portfolio Details Section ======= -->
+    <section id="portfolio-details" class="portfolio-details">
+      <style>
+        .portfolio-details .portfolio-description,
+        .portfolio-details {
+          padding-top: 0rem !important;
+        }
+
+        .text-success {
+          color: #00Af00 !important;
+        }
+      </style>
+      <?php
+      $first = true;
+      foreach ($portfolioItems as $item) {
+        if ($first == true) {
+
+          echo '<div class="portfolio-description"> <ul dir="rtl"  class="m-5">';
+
+          foreach ($item['description'] as $dis) {
+            echo "<li>$dis</li>";
+          }
+
+          echo '</ul></div>';
+          if (isset($item['dis']) && $item['dis'] != "") {
+            echo "<h6 class='text-center '><span class='border rounded border-success py-1 px-5' style='color:white ;  background-color: #043042 !important;'>" . $item['dis'] . "</span></h6>";
+          }
+          $first = false;
+        }
+        echo '<div class="container mb-5">
+        <div class="row gy-4">
+            <div class="col-lg-8">
+                <div class="portfolio-details-slider swiper">
+                    <div class="swiper-wrapper align-items-center">';
+
+        for ($i = 0; $i < $item['images']; $i++) {
+          $j = $i + 1;
+          echo '<div class="swiper-slide text-center" >
+                      <img src="' . $item['root'] . '(' . $j . ').jpg" alt="" class="img-fluid" style="max-height:50vh ;width:auto">
+                  </div>';
+        }
+        echo '</div>
+                    <div class="swiper-pagination"></div>
+                    
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="portfolio-info">
+                    <h3>' . $item['SD'] . '</h3>
+                    <ul dir="rtl" >
+                        <li><strong>الفئة</strong> : ' . $item['category'] . '</li>
+                        <li><strong>الوصف</strong> : ' . $item['SD'] . '</li>
+                        ';
+        if (isset($statusTexts[$item['projectStatu']])) {
+          echo "<li><strong>الحالة</strong> : " . $statusTexts[$item['projectStatu']] . "</li>";
+        } else {
+          if (isset($item['projectStatu']) && $item['projectStatu'] != "") {
+            echo "<li><strong>الحالة</strong> : " . $item['projectStatu'] . "</li>";
+          }
+        }
+        if (isset($item['area']) && $item['area'] != "") {
+          echo "<li><strong>المساحة التصميمية</strong> : " . $item['area'] . "</li>";
+        }
+        echo '      </ul>
+                </div>
+                
+            </div>
+        </div>
+    </div>';
+      }
+
+      ?>
+
+
+      </div>
+    </section><!-- End Portfolio Details Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <?php echo $footer; ?>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <?php echo $end; ?>
+
+</body>
+
+</html>
